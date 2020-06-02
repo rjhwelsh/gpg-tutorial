4891934a5d05c3ca346bcf8d8becabedfc50678e
Roger Welsh <rjhwelsh@gmail.com>
2020-06-03 01:06:18 +1200

    keysigning/list_key.sh: Simplify with grep

--- keysigning/list_key.sh
+++ keysigning/list_key.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# Lists key at the top of your keychain
+# Lists key at the bottom of your keychain (latest?)
 # OR A key you specify
-gpg -k --fingerprint $1 |head -n 4 |tail -n 1 |sed 's/ //g'|cut -c 25-
+gpg -k $1 |grep -Po '[0-9ABCDEF]{40}' |tail -n 1
