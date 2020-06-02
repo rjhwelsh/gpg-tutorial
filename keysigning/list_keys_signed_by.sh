7570a718d68c70350d66013aefea11aa3b3cd14a
Roger Welsh <rjhwelsh@gmail.com>
2020-06-03 01:50:34 +1200

    keysigning/list_keys_signed_by.sh: Exclude private keys
    from consideration

--- keysigning/list_keys_signed_by.sh
+++ keysigning/list_keys_signed_by.sh
@@ -2,6 +2,8 @@
 
 keyid=`./list_key.sh $1`
 
+private_keys=`gpg --with-colons -K|grep fpr|cut -d':' -f10`
+
 gpg --with-colons --fingerprint --list-sigs |
 while read line; do
   packettype="$(echo "${line}" | cut -d':' -f1)"
@@ -12,7 +14,10 @@ while read line; do
     sig)
       issuedby="$(echo "${line}" | cut -d':' -f5)"
       if [ "x${issuedby}" = "x${keyid}" ]; then
-        echo "${fingerprint}"
+	  # Exclude private keys from consideration
+	  echo "${private_keys}"|\
+	      grep "${fingerprint}" >/dev/null ||
+              echo "${fingerprint}"
       fi
       ;;
   esac
